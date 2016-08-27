30bb7933a4975a081865222c7cf0c2576e53929e
Halil Özgür <h@halil.im>
2016-08-27 04:30:52 +0300

    Get ready for Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :peepchat, Peepchat.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: System.get_env("DOMAIN"), port: 443],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
