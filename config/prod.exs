cb73b6978ae92229d00ece70f09dedf75eb74ab1
Halil Özgür <h@halil.im>
2016-08-27 04:49:20 +0300

    Improve Heroku config

--- config/prod.exs
+++ config/prod.exs
@@ -14,8 +14,7 @@ use Mix.Config
 config :peepchat, Peepchat.Endpoint,
   http: [port: {:system, "PORT"}],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  url: [host: System.get_env("DOMAIN"), port: 443],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [host: System.get_env("DOMAIN"), port: 443]
 
 # Do not print debug messages in production
 config :logger, level: :info
