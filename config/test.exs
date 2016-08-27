7a7be2cd42b732303b119eb20100d2784f8115b3
Halil Özgür <h@halil.im>
2016-08-27 05:43:54 +0300

    Try empty password for Travis Postgres

--- config/test.exs
+++ config/test.exs
@@ -13,7 +13,7 @@ config :logger, level: :warn
 config :peepchat, Peepchat.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "peepchat_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
