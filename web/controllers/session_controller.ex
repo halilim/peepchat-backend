679316ee13b1f0b3f3c576c49cecb0266b7da5db
Halil Özgür <h@halil.im>
2016-08-27 05:08:13 +0300

    Add Travis config

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -4,6 +4,6 @@ defmodule Peepchat.SessionController do
   def index(conn, _params) do
     # Return some static JSON for now
     conn
-    |> json(%{status: "Ok"})
+    |> json(%{status: "Ok, with travis!"})
   end
 end
